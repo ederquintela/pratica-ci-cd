fe91c32273c8e1dd24fac35d4a46c1363970f007
Eder Quintela <ederquintela@gmail.com>
2025-07-11 19:09:57 +0100

    inicio do projeto

---
+++ src/main/java/com/example/pratica_ci_cd/PraticaCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.pratica_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticaCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticaCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pratica_ci_cd/PraticaCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pratica_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticaCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
